c73520a0304bb21ad73da6a790ec0e7abde1e83e
helestia <helestia052@gmail.com>
2021-02-20 16:49:26 +0900

    debug: 未使用のログ表示を削除

--- src/components/commons/tabs/useTabs.ts
+++ src/components/commons/tabs/useTabs.ts
@@ -4,10 +4,8 @@ import {tTabState} from './tabs'
 const useTabs = (initSelected:string, tabs:tTabState[]) => {
     const [selected,setSelected] = React.useState(initSelected);
 
-    const initialize = () => {
-        console.log(initSelected);
-        setSelected(initSelected);
-    }
+    const initialize = () => setSelected(initSelected);
+    
     const handleChange = (event:React.ChangeEvent<{}>, selectedValue:string) => {
         const selectedTab = tabs.find(tab => tab.value === selectedValue);
         if(selectedTab === undefined) return;
